7fe79e89b3bd26499e84525f858c031e145a4055
marco mason <marco.mason@gmail.com>
2020-04-26 12:28:02 -0400

    Resize display based on screen size; toggle menu/logo

--- src/asciilloscope.pl
+++ src/asciilloscope.pl
@@ -12,16 +12,28 @@ my $key;                        #
 
 my $VERSION=0.03;
 
+my @LOGO = (
+    '   _    ___   __  _______  _    ___   ___   __   ___   ___  ___',
+    '  /_\  / __| / _||_ _|_ _|| |  / _ \ / __| / _| / _ \ |  _\| __|',
+    ' / _ \ \__ \| (_  | | | | | |_| (_) |\__ \| (_ | (_) || |_/| _| ',
+    '/_/ \_\|___/ \__||___|___||___|\___/ |___/ \__| \___/ |_|  |___|'." v$VERSION",
+);
+
 # display parameters stored in hash for future conversion into an
 # object orientated 
 my %display=(                  # display parameters
+   showMenu  =>1,              # display menu on right
+   showLogo  =>1,              # display logo below scope
    borderStyle=>"double",      # border style
-   height    =>14,             # vertical characters
-   width     =>50,             # horizontal characters
+   #height    =>14,             # Scope display size (vertical characterss)
+   #width     =>50,             # Scope display size (horizontal characters)
+   #screen_width=>80,           # Screen size
+   #screen_height=>24,
    row       =>2,              # vertical position (from top left)
    column    =>10,             # horizontal position
    sampleRate=>100,            # number of samples per second
-   symbol    =>"*",);              # plot symbol
+   symbol    =>"*",            # plot symbol
+);
 
 my %actions=(                  # for keyboard driven actions
    113=>{ # q exits
@@ -64,8 +76,22 @@ my %actions=(                  # for keyboard driven actions
 	   note=>"- = Reduce",
 	   proc=>sub{$display{yMult}*=0.9;},
    },
-   
+   108=>{ # Toggle logo display
+       note=>"L = Hide/Show logo",
+       proc=>sub{toggleDisplayFlag("showLogo");},
+   },
+   63=>{ # Toggle menu display
+       note=>"? = Hide/Show menu",
+       proc=>sub{toggleDisplayFlag("showMenu");},
+   },
 );
+
+# Toggle a display flag, reconfigure the screen and redisplay
+sub toggleDisplayFlag {
+    my $flag = shift;
+    $display{$flag} = !$display{$flag};
+    initialScreen();
+}
 
 # example initial dataset...a sine wave preloaded to allow scaling -1 to 1
 # subsequent data can be autoscaled again as required.
@@ -74,6 +100,7 @@ push @list,sin (3.14*$_/20) for (0..55);
 my $next=@list;
 
 # Main routine
+initialSetup();
 initialScreen();   # draw screen
 autoLevels();      # auto adjust the scaling based on initial sample
 startScope();      # the loop that updates the scope's display
@@ -85,6 +112,10 @@ sub initialScreen{
         simple=>{tl=>"+", t=>"-", tr=>"+", l=>"|", r=>"|", bl=>"+", b=>"-", br=>"+",},
         double=>{tl=>"╔", t=>"═", tr=>"╗", l=>"║", r=>"║", bl=>"╚", b=>"═", br=>"╝",},
     );
+
+    setDisplaySize();
+    clearDisplayArea();
+
     my %border=%{$borders{$display{borderStyle}}};
 	foreach (0..$display{height}){
 		$plotArea[$_]=$border{l}.(" "x$display{width}).$border{r};
@@ -93,15 +124,16 @@ sub initialScreen{
     push    @plotArea,$border{bl}.($border{b}x$display{width}).$border{br};
     printAt($display{row},$display{column},@plotArea);
 
-    printAt( 3,$display{width}+$display{column}+3,
-        map{$actions{$_}{note} } sort { $a <=> $b } keys %actions);
-    
-    printAt($display{row}+$display{height}+3,$display{column}-7<0?0:$display{column}-7,
-    '   _    ___   __  _______  _    ___   ___   __   ___   ___  ___',
-    '  /_\  / __| / _||_ _|_ _|| |  / _ \ / __| / _| / _ \ |  _\| __|',
-    ' / _ \ \__ \| (_  | | | | | |_| (_) |\__ \| (_ | (_) || |_/| _| ',
-    '/_/ \_\|___/ \__||___|___||___|\___/ |___/ \__| \___/ |_|  |___|'." v$VERSION",
-    );
+    if ($display{showMenu}) {
+        printAt( 3,$display{width}+$display{column}+3,
+            map{$actions{$_}{note} } sort { $a <=> $b } keys %actions);
+    }
+
+    if ($display{showLogo}) {
+        printAt($display{row}+$display{height}+3,$display{column}-7<0?0:$display{column}-7,
+            @LOGO
+        );
+    }
 }
 
 # uses the data in the @list to autscale the waveform for display
@@ -163,3 +195,60 @@ sub bound{
 	return $min if $number<$min;
 	return $number;	
 }
+
+# Examine system and configure the ascilloscope data
+sub initialSetup {
+    getScreenSize();
+    #$display{showMenu} = 1;
+
+    #borderStyle=>"double",      # border style
+    #height    =>14,             # vertical characters
+    #width     =>50,             # horizontal characters
+    #row       =>2,              # vertical position (from top left)
+    #column    =>10,             # horizontal position
+    #sampleRate=>100,            # number of samples per second
+    #symbol    =>"*",);              # plot symbol
+    #setDisplaySize();
+    #clearDisplayArea();
+}
+
+sub clearDisplayArea {
+    my $t = " " x ($display{screen_width}-1);
+    printAt(1, 1, ($t) x ($display{row}+$display{height}+2) );
+}
+
+# Configure display size (depending on whether menu is displayed or not, and
+# the screen space available)
+sub setDisplaySize {
+    my $menu_width = 0;
+    if ($display{showMenu}) {
+        for my $rAct (values %actions) {
+            $menu_width = length($rAct->{note}) if $menu_width < length($rAct->{note});
+        }
+    }
+    $display{width} = $display{screen_width} - ($display{column}+$menu_width) - 2;
+    $display{height} = $display{screen_height} - 10;
+    if ($display{showLogo}) {
+        $display{height} -= @LOGO;
+    }
+}
+
+# Try to find the screen size
+sub getScreenSize {
+    # The default values we'll use if we can't find something better
+    my ($width, $height) = (80,24);
+    no strict;
+    if (my @tmp = GetTerminalSize(STDOUT)) {
+        # Term::ReadKey gave us a screen size
+        $width = $tmp[0];
+        $height = $tmp[1];
+    }
+    else {
+        # Many systems use ENV settings (Cygwin, *nix, ...)
+        $width = $ENV{COLUMNS}  if defined $ENV{COLUMNS};
+        $height = $ENV{ROWS}    if defined $ENV{ROWS};
+    }
+    $display{screen_width} = $width;
+    $display{screen_height} = $height;
+}
+
